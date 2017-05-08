db9f54fd046c5195ceac3df7e8d4a9fe11a29dd8
Kacper Kaśków <kacper.kaskow@apilia.pl>
2017-05-08 16:40:59 +0800

    add widget

---
+++ app/src/main/java/com/udacity/stockhawk/widget/DetailWidgetRemoteViewsService.java
@@ -0,0 +1,143 @@
+package com.udacity.stockhawk.widget;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.os.Binder;
+import android.os.Build;
+import android.widget.AdapterView;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.data.Contract;
+import com.udacity.stockhawk.data.PrefUtils;
+import com.udacity.stockhawk.ui.MainActivity;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class DetailWidgetRemoteViewsService extends RemoteViewsService {
+    public final String LOG_TAG = DetailWidgetRemoteViewsService.class.getSimpleName();
+    // these indices must match the projection
+    static final int INDEX_WEATHER_ID = 0;
+
+    private DecimalFormat dollarFormatWithPlus;
+    private DecimalFormat dollarFormat;
+    private DecimalFormat percentageFormat;
+
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new RemoteViewsFactory() {
+            private Cursor data = null;
+
+            @Override
+            public void onCreate() {
+                dollarFormat = (DecimalFormat) NumberFormat.getCurrencyInstance(Locale.US);
+                dollarFormatWithPlus = (DecimalFormat) NumberFormat.getCurrencyInstance(Locale.US);
+                dollarFormatWithPlus.setPositivePrefix("+$");
+                percentageFormat = (DecimalFormat) NumberFormat.getPercentInstance(Locale.getDefault());
+                percentageFormat.setMaximumFractionDigits(2);
+                percentageFormat.setMinimumFractionDigits(2);
+                percentageFormat.setPositivePrefix("+");
+            }
+
+            @Override
+            public void onDataSetChanged() {
+                if (data != null) {
+                    data.close();
+                }
+                final long identityToken = Binder.clearCallingIdentity();
+                data = getContentResolver().query(Contract.Quote.URI,
+                        Contract.Quote.QUOTE_COLUMNS.toArray(new String[]{}),
+                        null,
+                        null,
+                        null);
+                Binder.restoreCallingIdentity(identityToken);
+            }
+
+            @Override
+            public void onDestroy() {
+                if (data != null) {
+                    data.close();
+                    data = null;
+                }
+            }
+
+            @Override
+            public int getCount() {
+                return data == null ? 0 : data.getCount();
+            }
+
+            @Override
+            public RemoteViews getViewAt(int position) {
+                if (position == AdapterView.INVALID_POSITION ||
+                        data == null || !data.moveToPosition(position)) {
+                    return null;
+                }
+                RemoteViews views = new RemoteViews(getPackageName(), R.layout.widget_detail_list_item);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
+//                    setRemoteContentDescription(views, description);
+                }
+                //----------------------------------------------------------------------------------
+                views.setTextViewText(R.id.symbol, data.getString(Contract.Quote.POSITION_SYMBOL));
+                views.setTextViewText(R.id.price, dollarFormat.format(data.getFloat(Contract.Quote.POSITION_PRICE)));
+
+
+                float rawAbsoluteChange = data.getFloat(Contract.Quote.POSITION_ABSOLUTE_CHANGE);
+                float percentageChange = data.getFloat(Contract.Quote.POSITION_PERCENTAGE_CHANGE);
+
+                if (rawAbsoluteChange > 0) {
+                    views.setInt(R.id.change, "setBackgroundColor", Color.GREEN);
+                } else {
+                    views.setInt(R.id.change, "setBackgroundColor", Color.RED);
+                }
+
+                String change = dollarFormatWithPlus.format(rawAbsoluteChange);
+                String percentage = percentageFormat.format(percentageChange / 100);
+
+                if (PrefUtils.getDisplayMode(getApplicationContext())
+                        .equals(getApplicationContext().getString(R.string.pref_display_mode_absolute_key))) {
+                    views.setTextViewText(R.id.change, change);
+                } else {
+                    views.setTextViewText(R.id.change, percentage);
+                }
+                //----------------------------------------------------------------------------------
+
+                final Intent fillInIntent = new Intent(getApplicationContext(), MainActivity.class);
+                views.setOnClickFillInIntent(R.id.widget_list, fillInIntent);
+                return views;
+            }
+
+            @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
+            private void setRemoteContentDescription(RemoteViews views, String description) {
+//                views.setContentDescription(R.id.widget_icon, description);
+            }
+
+            @Override
+            public RemoteViews getLoadingView() {
+                return new RemoteViews(getPackageName(), R.layout.widget_detail_list_item);
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 1;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                if (data.moveToPosition(position))
+                    return data.getLong(INDEX_WEATHER_ID);
+                return position;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return true;
+            }
+        };
+    }
+}
